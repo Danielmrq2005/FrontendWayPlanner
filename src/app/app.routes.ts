da930bd3a033e973c7cd617a055e29651ab10d9b
Danielmrq2005 <148747187+Danielmrq2005@users.noreply.github.com>
2025-04-25 09:13:49 +0200

    frontend provisional

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,8 +6,22 @@ export const routes: Routes = [
     loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
   },
   {
+    path: 'crear-viaje',
+    loadComponent: () => import('./crear-viaje/crear-viaje.component').then((m) => m.CrearViajeComponent),
+  },
+
+  {
+    path: 'detalles',
+    loadComponent: () => import('./detalles-viaje/detalles-viaje.component').then((m) => m.DetallesViajeComponent),
+  },
+  {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'viajes',
     pathMatch: 'full',
+  },
+
+  {
+    path: 'viajes',
+    loadComponent: () => import('./viajes/viajes.component').then((m) => m.ViajesComponent),
   },
 ];
