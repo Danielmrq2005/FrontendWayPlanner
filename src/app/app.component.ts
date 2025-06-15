fb4dd322778309d2b81a137f90a6aab5fe0d018c
Danielmrq2005 <148747187+Danielmrq2005@users.noreply.github.com>
2025-06-15 23:06:18 +0200

    desplegando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,14 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 import {NotificacionPopupComponent} from "./notificacion-popup/notificacion-popup.component";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet, NotificacionPopupComponent,BrowserAnimationsModule],
+  imports: [IonApp, IonRouterOutlet, NotificacionPopupComponent],
   standalone: true,
 })
 export class AppComponent {
-  constructor() {
-
-  }
+  constructor() {}
 }
